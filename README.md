f5b4d5a11f6166b42ee737b4675d51a6ed23f255
Thomas Houtrique <thomas@MacBook-Air-de-Thomas.local>
2025-01-07 15:16:19 +0100

    fix unittest

---
+++ README.md
@@ -0,0 +1,13 @@
+![Python](https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&logoColor=ffdd54)
+
+# Projet Secu Houtrique Thomas
+
+## Éxecution du script
+```bash
+python3 main.py
+```
+
+## Unittest
+```bash
+python3 test_unit.py
+```
--- test_unit.py
+++ test_unit.py
@@ -5,7 +5,7 @@ from password import Password
 class TestPassword(unittest.TestCase):
     def test_password_length(self):
         password: str = Password(password="password")
-        self.assertEqual(password.check_length(), False)
+        self.assertEqual(password.check_length(), True)
 
 if __name__ == "__main__":
     unittest.main()
